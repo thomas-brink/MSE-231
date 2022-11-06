5962b4d963ff7a41246bbacc53ed9ac35457d8bf
Eva Batelaan <batelaan@stanford.edu>
2022-11-05 19:10:57 -0700

    style

--- assignment2/parse_congress_data.py
+++ assignment2/parse_congress_data.py
@@ -1,3 +1,22 @@
+"""This code was written by
+        Eva Batelaan <batelaan@stanford.edu>
+        Thomas Brink <tbrink@stanford.edu>
+        Michelle Lahrkamp <ml17270@stanford.edu>
+    Assignment 2 Group 3
+
+    Use:
+    python3 parse_congress_data.py
+    
+    Requirements:
+    Must have downloaded and saved the following files from 
+    [congress-legislators](https://github.com/unitedstates/congress-legislators):
+        'legislators-current.yaml'
+        'legislators-social-media.yaml'
+    
+    Output:
+    Creates a date-stamped file congress_df_YYYY-MM-DD.csv where YYYY-MM-DD
+    is the date that the 'legislators-current.yaml' file was downloaded
+"""
 import numpy as np
 import pandas as pd
 import yaml
@@ -5,15 +24,16 @@ import datetime
 import os
 
 def read_yaml(filename):
+    """Read in data safely from file.
+    """
     with open(filename, 'r') as f:
         list_data = yaml.safe_load(f)
-
-    return list_data
+        return list_data
 
 def get_party(terms, today):
-    """ Blablabla
+    """Gets the legislator's current party affiliation based on their current term
     """
-    for term in leg['terms']:
+    for term in terms:
         start_date = datetime.datetime.strptime(term['start'], '%Y-%m-%d').date()
         end_date = datetime.datetime.strptime(term['end'], '%Y-%m-%d').date()
 
@@ -22,7 +42,7 @@ def get_party(terms, today):
 
 
 def get_twitter_info(leg_soc, leg_bio_id):
-    """ Blablabla
+    """Gets twitter_id,twitter_username for a given legislator
     """
     leg_soc_ids = np.array([leg_soc[i]['id']['bioguide'] for i in range(len(leg_soc))])
     try:
@@ -44,9 +64,9 @@ if __name__ == "__main__":
     for leg in leg_cur:
         party = get_party(leg['terms'], date)
         twitter_id, twitter_username = get_twitter_info(leg_soc, leg['id']['bioguide'])
-
         if twitter_id:
+            # Only save the legislator if twitter account could be found
             congress_df = congress_df.append({'twitter_id': twitter_id, 'twitter_username': twitter_username,
                                               'party': party}, ignore_index = True)
-
+    # Output to date-stamped csv file
     congress_df.to_csv('congress_df_{}.csv'.format(date.strftime("%Y-%m-%d")))
