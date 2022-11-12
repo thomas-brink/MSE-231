802d27813aa0faa976f5cf50f4700cf985bf59af
Eva Batelaan <batelaan@stanford.edu>
2022-11-12 12:42:25 -0800

    partially working model stuff

--- assignment3/survey_poststrat.py
+++ assignment3/survey_poststrat.py
@@ -16,6 +16,40 @@ import numpy as np
 from sklearn.linear_model import LogisticRegression
 
 if __name__ == "__main__":
+    # Combine survey data
     og_survey_df = pd.read_csv('comma-survey.csv', index_col='RespondentID')
     new_survey_df = pd.read_csv('new_comma_survey.csv', index_col='RespondentID')
     survey_df = pd.concat([og_survey_df, new_survey_df])
+    
+    # Extract question and demographics columns
+    question_cols = list(survey_df.columns)[:7]
+    demographic_cols = list(survey_df.columns)[7:11]
+    
+    # Encode demographic responses
+    demographics = survey_df[demographic_cols].to_numpy()
+    # Define the ordinal features pipeline
+    enc = Pipeline([
+        ('imputer', SimpleImputer(strategy='most_frequent')),
+        ('encoder', OrdinalEncoder()),
+        ('scaler', StandardScaler())
+    ])
+    enc.fit(demographics)
+    X_demographics = enc.transform(demographics)
+    
+    # Fit encoders of question responses
+    labelEncoders = []
+    for q in question_cols:
+        y_vals = survey_df[q].to_numpy()
+        le = preprocessing.LabelEncoder()
+        le.fit(y_vals)
+        labelEncoders.append(le)
+        
+    # Fit models
+    lr = LogisticRegression(multi_class='multinomial')
+    models = []
+    for idx, q in enumerate(question_cols):
+        y_vals = survey_df[q].to_numpy()
+        Y_vals = labelEncoders[idx].transform(y_vals)
+        model = lr.fit(X_demographics, Y_vals)
+        models.append(model)
+    
